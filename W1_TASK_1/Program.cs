a84ad40fbd8a9df9e4c61102e338de14a670ac7d
SEMIP <SEMIP@DESKTOP-1>
2025-06-28 02:14:40 +0300

    Update

--- W1_TASK_1/Program.cs
+++ W1_TASK_1/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace W1_TASK_1
 {
+    //Salah Eldin Mohamad Hussein
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to login page");
+            Console.WriteLine("please Enter Username:");
+            string username = Console.ReadLine();
+            Console.WriteLine("please Enter Password:");
+            string password = Console.ReadLine();
+            if (username == "admin" && password == "admin")
+                Console.WriteLine("You are logged in");
+            else
+                Console.WriteLine("Wrong Username or Password");
+
         }
     }
 }
