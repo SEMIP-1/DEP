147b4add553617f903133b2a6a3139a028f28fb3
Salah Eldin Mohamed <salah.ahmed99@eng-st.cu.edu.eg>
2025-09-02 11:19:47 +0300

    update

---
+++ 2025.08.29/Task.cs
@@ -0,0 +1,18 @@
+//Task OOP second session: 
+//1.	Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+//2.	Create a struct called "Point" to represent a 2D point with properties "X" and   "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+
+
+//3.	Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
+
+//4.Create a struct named Rectangle that represents a rectangle with the following fields:
+//width (type: double)
+//height (type: double)
+
+//Implement encapsulation by making the fields private and provide public properties  access and modify these values. Ensure the following conditions are met:
+//The width and height should not be negative. If a negative value is provided, the setter should not update the field and should instead print an error message.
+//Implement a public read-only property Area that calculates and returns the area of the rectangle (Area = width * height).
+
+//Implement a method DisplayInfo that prints the rectangle's dimensions and area.
+//Write a program to demonstrate the usage of this struct by creating an instance, setting values via properties, and displaying the area.
